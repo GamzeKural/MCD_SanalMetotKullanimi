18b4dc66987cc08ad194d1c17429dc62814cd2a8
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-03 17:17:01 +0300

    Add project files.

---
+++ MCD_SanalMetotKullanimi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            televizyon T1 = new televizyon();
+            T1.ekranaYaz("Hangisi? ");
+            
+            Console.WriteLine("*****************");
+           
+            urun U1 = new urun();
+            U1.ekranaYaz("Merhaba");
+           
+            Console.WriteLine("*****************");
+            
+            baseClass B1 = new baseClass();
+            B1.ekranaYaz("Merhaba");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MCD_SanalMetotKullanimi/televizyon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    public class televizyon:urun
+    {
+        public televizyon()
+        {
+            Console.WriteLine("televizyon");
+        }
+        
+    }
+}
